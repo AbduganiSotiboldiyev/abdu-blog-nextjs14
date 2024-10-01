4a1c3ab9cdc5f0e1ce6c7fe5dfcbb1c28ac87b97
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-10-01 19:13:57 +0900

    Share buttons

--- app/(root)/blogs/[slug]/page.tsx
+++ app/(root)/blogs/[slug]/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from '@/components/ui/button'
+
 import { getReadingTime } from '@/lib/utils'
 import { getDetailedPage } from '@/service/blog.service'
 import { format } from 'date-fns'
 import parse from 'html-react-parser'
-import { ArrowUpRight, CalendarDays, Clock, Facebook, Link2, Linkedin, Minus, Send, Twitter } from 'lucide-react'
+import { ArrowUpRight, CalendarDays, Clock,  Minus} from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import ShareButtons from '../../_components/share-buttons'
 
 
 export async function generateMetadata({params} : {params : {slug: string}}) {
@@ -58,26 +59,7 @@ async function SlugPage({params} : {params : {slug: string}}) {
             />
             <div className='flex md:gap-12 max-md:flex-col-reverse mt-12 relative'>
               <div className='flex flex-col space-y-3'>
-                <div className='sticky top-36'>
-                  <p className='text-lg uppercase text-muted-foreground'>Share</p>
-                  <div className='flex flex-col max-md:flex-row md:space-y-3 max-md:space-x-3 mt-4'>
-                    <Button size={'icon'} variant={'outline'}>
-                      <Twitter />
-                    </Button>
-                    <Button size={'icon'} variant={'outline'}>
-                      <Facebook />
-                    </Button>
-                    <Button size={'icon'} variant={'outline'}>
-                      <Linkedin />
-                    </Button>
-                    <Button size={'icon'} variant={'outline'}>
-                      <Send />
-                    </Button>
-                    <Button size={'icon'} variant={'outline'}>
-                      <Link2/>
-                    </Button>
-                  </div>
-                </div>
+                <ShareButtons/>
               </div>
               <div className='flex-1 prose dark:prose-invert'>
                 {parse(blog.info.html)}
